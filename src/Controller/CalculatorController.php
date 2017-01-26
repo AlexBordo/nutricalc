1a3521b4272b401f9ca1a040fba3ccb4e55683e3
AlexBordo <alexbursak3@gmail.com>
2017-01-26 19:32:38 +0000

    [UPDATE] - implemented validator and refactor.

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -2,10 +2,27 @@
 
 namespace NutriCalc\Controller;
 
+use NutriCalc\Type\Calculator;
+use NutriCalc\Type\JsonValidator;
+
 class CalculatorController
 {
     public function calculateAction()
     {
-       
+        $data = file_get_contents('php://input');
+
+        $validator = new JsonValidator($data);
+
+        if(!$validator->isValidateFields()){
+            echo json_encode((object)$validator->getErrors());
+            return;
+        }
+
+        $data = json_decode($data);
+
+        $calc = new Calculator((array)$data);
+
+        var_dump($calc);
     }
+
 }
