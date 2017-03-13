f6e9ff629e937b6b695a68631a310bc9cb400086
alexbursak <alexbursak3@gmail.com>
2017-03-13 14:15:08 +0000

    [UPDATE] - implemented simple arrayCollection component + tests

---
+++ src/Component/ArrayCollection/ArrayCollection.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NutriCalc\Component\ArrayCollection;
+
+/**
+ * Class ArrayCollection
+ */
+class ArrayCollection
+{
+    /**
+     * @var array
+     */
+    public $elements = [];
+
+    /**
+     * @param mixed $element
+     *
+     * @return $this
+     */
+    public function addElement($element)
+    {
+        array_push($this->elements, $element);
+
+        return $this;
+    }
+
+    /**
+     * @param array $elements
+     *
+     * @return $this
+     */
+    public function addElements(array $elements)
+    {
+        foreach ($elements as $element) {
+            array_push($this->elements, $element);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getElements()
+    {
+        return $this->elements;
+    }
+}
